7d2d3bf66b3465d5290698f481b02e3ac9cfa6db
rigofekete <fabricecosteira@gmail.com>
2025-07-07 11:12:19 +0200

    fix to make sure input video size values are mul of 2 as required by x264 encoder. added dpi scaling awarness

--- src/recording.py
+++ src/recording.py
@@ -1,17 +1,18 @@
 import subprocess
 import av
-# import numpy as np
+import numpy as np
 import os
 import fractions
 import time
 import win32gui
 import ctypes
+import datetime
 
 # COMMAND TO RUN FFMPEG FROM WINDOWS, WITH MAX COMPATIBILITY, FOR 15 SECONDS 
 
 # ffmpeg.exe -f gdigrab -framerate 30 -i desktop -c:v libx264 -preset fast -crf 23 -pix_fmt yuv420p -profile:v baseline -level 3.0 -t 15 output.mp4
 
-# COMMAND TO RUN FFMPEG WITH USING NVIDIA GPU AS THE CAPTURE SOURCE FOR PROPER QUALITY AND FPS RECORDING
+# COMMAND TO RUN FFMPEG USING NVIDIA GPU AS THE CAPTURE SOURCE (ddagrab) FOR PROPER QUALITY AND FPS RECORDING
 
 # >ffmpeg -f lavfi -i "ddagrab=framerate=60" -c:v h264_nvenc -cq 18 -y output.mp4
 
@@ -34,8 +35,12 @@ import ctypes
 def get_dpi_aware_window_rect(title):
     hwnd = win32gui.FindWindow(None, title)
     if not hwnd:
+        print("Window not found")
         return None
 
+    win32gui.ShowWindow(hwnd, 1)
+    win32gui.SetForegroundWindow(hwnd)
+    
     # Get window rect (in physical pixels)
     rect = win32gui.GetWindowRect(hwnd)
     x, y, r, b = rect
@@ -48,16 +53,30 @@ def get_dpi_aware_window_rect(title):
         user32 = ctypes.windll.user32
         dpi = user32.GetDpiForWindow(hwnd)
     except AttributeError:
+        print("user32 and dpi value returns with ctypes failed")
         # Fallback to standard DPI
-        dpi = 96
+
+
+    #### DPI SCALE CALCULATION
+    # scale = dpi / 96.0: Calculates the DPI scaling factor. Since 96 DPI is the Windows standard baseline:
+    # 96 DPI = 1.0 scale (100% scaling)
+    # 120 DPI = 1.25 scale (125% scaling)
+    # 144 DPI = 1.5 scale (150% scaling)
+    # 192 DPI = 2.0 scale (200% scaling)
 
     scale = dpi / 96.0
-    x = int(x * scale) + 10
-    y = int(y * scale) + 10
-    width = int(width * scale)
-    height = int(height * scale)
-    return x, y, x + width, y + height
 
+
+    x = int(x * scale) + 10             # add/sub values to fine-tune capturing x, y, w, h regions
+    y = int(y * scale) + 20             # add/sub values to fine-tune capturing x, y, w, h regions
+    width = int(width * scale) - 20     # add/sub values to fine-tune capturing x, y, w, h regions
+    height = int(height * scale) -20    # add/sub values to fine-tune capturing x, y, w, h regions
+
+    # we need to make sure that the w and h values are even! 
+    # (libx264 requires width/height divisible by 2)
+    width = width - (width % 2)
+    height = height - (height % 2)
+    return (x, y, x + width, y + height)
 
 class ScreenRecorder:
     def __init__(self):
@@ -72,53 +91,58 @@ class ScreenRecorder:
     def capture_frames(self):
         # window_title ="Pro Evolution Soccer 2" 
         # window_title ="desktop" 
-        # rect = get_window_rect("Calculator")
-        rect = get_dpi_aware_window_rect("Calculator")
+
+        # Apparently certain window hwnd's like these 2 ones below seem to have restrictions for capture
+        # and restore (ShowWindow() function for example), need to investigate this further
+        # rect = get_dpi_aware_window_rect("Calculator")
+        # rect = get_dpi_aware_window_rect("Steam")
+
+        rect = get_dpi_aware_window_rect("PCSX2 v2.2.0")
+        # rect = get_dpi_aware_window_rect("Pro Evolution Soccer 2")
+        # rect = get_dpi_aware_window_rect("WE6FE Classicos 2.0 (Hack Ed)")
         if rect:
-            print(f"Calculation rect....")
+            print(f"Calculating rect....")
             x, y, right, bottom = rect
             width =  right - x
             height = bottom - y
+            print(f"width {width} height {height}")
+        else:
+            print("rect is null")
 
+        video_size = f'{width}x{height}'
 
         self.input_container = av.open('desktop',
-                                        # f"title={window_title}",
                                         format='gdigrab',
                                         mode='r',
                                         options={
                                             'framerate': '30',
-                                            'show_region': '0',
                                             'offset_x': str(x),
                                             'offset_y': str(y),
-                                            # 'video_size': '1920x1080'
-                                            'video_size': f'{width}x{height}',  # Your 4K resolution
-                                            # 'show_region': '0',
+                                            # 'video_size': video_size,  
+                                            # 'video_size': '1920x1080',  
+                                            'video_size': f'{width}x{height}',  
+                                            'show_region': '0',
                                             # 'draw_mouse': '0',
-                                            # 'blocksize': '1048576',  # 1MB buffer
-                                            # 'probesize': '10M'
                                         }
                                 )
-        print(f"container:{dir(av.open)}")
 
 
     def start_recording(self):
         if not self.recording:
             try:
-                output_path = os.path.abspath('output2.mp4')
+                now = str(datetime.datetime.now()).replace(' ', '_').replace(':', '-')
+                output_path = os.path.abspath(f'output\\{now}.mp4')
                 self.output_container = av.open(output_path, 'w')
 
                 self.output_stream = self.output_container.add_stream('libx264', rate=30)
                 self.output_stream.width = self.input_container.streams.video[0].width
                 self.output_stream.height = self.input_container.streams.video[0].height
-                # self.output_stream.width = 1920
-                # self.output_stream.height = 1080
                 self.output_stream.pix_fmt = 'yuv420p'
                 # self.output_stream.codec_context.time_base = fractions.Fraction(1, 30)
                 # self.output_stream.time_base = fractions.Fraction(1, 60)
                 self.output_stream.codec_context.options = {
-                    'preset': 'fast',
-                    'rc': 'cbr',
-                    'bitrate': '5000k'
+                    # 'rc': 'cbr',
+                    # 'bitrate': '5000k'
                     # 'level': '3.0',
                     # 'preset': 'medium',        # Changed from 'ultrafast' - better quality
                     # 'tune': 'zerolatency',     # Keep for real-time
@@ -138,7 +162,6 @@ class ScreenRecorder:
                     # 'aq_mode': '1',            # Adaptive quantization
                     # 'aq_strength': '1.0'       # AQ strength
                 }
-                print(f"stream :{dir(self.output_stream)}")
 
                 self.recording = True
                 print("Recording started")
@@ -161,14 +184,12 @@ class ScreenRecorder:
     def process_frames(self):
         if not self.recording:
             self.start_recording()
-
-        # pts = 0
+        
         time_base = fractions.Fraction(1, 30)
         self.output_stream.time_base = time_base
 
         start_time = time.time()
         last_pts = -1
-
 
         frame_index = 0 
 
@@ -189,6 +210,7 @@ class ScreenRecorder:
                         frame.time_base = time_base
 
                         packet = self.output_stream.encode(frame)
+
                         self.output_container.mux(packet)
 
                         last_pts = current_pts 
